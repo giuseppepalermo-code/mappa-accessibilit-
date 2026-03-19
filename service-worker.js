b5c8d7e0cd92611bb2053f16bca61c3c6b4354d4
giuseppepalermo-code <giuseppevalerio.palermo@gmail.com>
2026-03-19 01:29:11 +0100

    correzione cache service worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,42 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker installato");
+const CACHE_NAME = "mappa-accessibilita-v2";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", () => {
-  // versione base
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
